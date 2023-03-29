6d89ad44a6f1253d99d15060c8f65e8ffde46902
manuel parada <xkiller838@gmail.com>
2023-03-29 11:49:42 -0500

    primera carga

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { createServer } = require("http");
+const realtimeServer = require("./realtimeServer");
+
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const httpServer = createServer(app);
+
+// Settings
+app.set("port", process.env.PORT  || 3000);
+app.set("views", path.join(__dirname, "views"))
+app.use(cookieParser());
+
+//Routes
+app.use( require("./routes"));
+
+//Public 
+app.use(express.static(path.join(__dirname, "public")));
+
+//Levanto el servidor
+httpServer.listen(app.get("port"), () =>{
+  console.log("El servidor esta corriendo en el puerto", app.get("port"));
+})
+
+// Llamo al servidor de socket.io
+realtimeServer(httpServer);
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+const views = path.join(__dirname, "/../views");
+
+const isLoggedIn = require("../middlewares/isLoggedIn");
+
+router.get("/", isLoggedIn, (req, res) =>{
+    res.sendFile(views + "/index.html");
+});
+
+router.get("/register", (req, res) => {
+    res.sendFile(views + "/register.html");
+});
+
+module.exports = router;
